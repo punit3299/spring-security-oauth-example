bbaaf3756006bc98a5bb4b368c03364303f23ffa
Punit Raj <raj.punit3299@gmail.com>
2021-04-18 23:29:31 +0530

    project init

---
+++ src/main/java/com/spring/fb/oauth/SpringSecurityFacebookOauthExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.fb.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFacebookOauthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFacebookOauthExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/fb/oauth/SpringSecurityFacebookOauthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.fb.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFacebookOauthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
