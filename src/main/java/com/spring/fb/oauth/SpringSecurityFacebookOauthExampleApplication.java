9bf9fb5dde93b7aaa2846d6d6aef15fe33e9f8f6
Punit Raj <raj.punit3299@gmail.com>
2021-04-18 23:46:42 +0530

    oauth endpoint added

--- src/main/java/com/spring/fb/oauth/SpringSecurityFacebookOauthExampleApplication.java
+++ src/main/java/com/spring/fb/oauth/SpringSecurityFacebookOauthExampleApplication.java
@@ -1,11 +1,27 @@
 package com.spring.fb.oauth;
 
+import java.security.Principal;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableOAuth2Sso
 public class SpringSecurityFacebookOauthExampleApplication {
 
+	@GetMapping("/")
+	public String fbLogin(Principal principal) {
+		Map<String,Object> details = (Map<String, Object>) ((OAuth2Authentication)principal).getUserAuthentication().getDetails();
+		String username = details.get("name").toString();
+		return "Hello "+username;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityFacebookOauthExampleApplication.class, args);
 	}
